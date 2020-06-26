29f7c81d3ddb9241e4f1bdd4f1031a1b195e4c71
Aaron Tobias <timelessrock3r23@gmail.com>
2020-06-25 23:46:36 -0400

    set up rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("challenge.bin")?;
+    let rdr = BufReader::new(file);
+    for line in rdr.lines(){
+        println!("{}",line?);
+    }
+    Ok(())
 }
