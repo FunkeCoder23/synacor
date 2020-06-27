ba256ca6c776be074e93b4d7b1999376b4abe943
funkecoder23 <timelessrock3r23@gmail.com>
2020-06-27 16:24:32 -0400

    renamed archspec, added VM struct

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() -> std::io::Result<()> {
     let file = File::open("challenge.bin")?;
     let rdr = BufReader::new(file);
     for line in rdr.lines(){
-        println!("{}",line?);
+        println!("{:?} ",line?);
     }
     Ok(())
 }
